f466c59f6b23ce3a6a4be0e5e9f3c4cf469a4e84
Juan Ignacio Gil Palacios <jgilpalacios@users.noreply.github.com>
2017-11-30 17:29:21 +0100

    inicial1

--- www/js/maps.js
+++ www/js/maps.js
@@ -15,7 +15,7 @@ var app = {
   pintaCoordenadasEnMapa: function(position){
     var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamdpbHBhbGFjaW9zIiwiYSI6ImNqYW1vdTIzNzRldG4yd3A3OTU2dmxpZ2IifQ.VhGDMhzfMAndfDrZ9QkFeg', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(miMapa);
